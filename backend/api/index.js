bf178a866d41a6d87f74034a28adfd795de62230
alphagit88-lab <alphagit88@gmail.com>
2026-02-01 12:50:53 +0530

    Fix Vercel serverless function routing for Express.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -5,5 +5,5 @@ require('dotenv').config();
 
 const app = require('../src/app');
 
-// Export the Express app as a serverless function
+// Export the Express app as a serverless function handler
 module.exports = app;
